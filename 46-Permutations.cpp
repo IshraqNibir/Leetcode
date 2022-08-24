f12679922f522a14e40e1ccd5a925146b61411a5
Ishraq_Nibir <ishraq.nibir@gmail.com>
2022-08-24 22:24:02 +0600

    Add files via upload

---
+++ 46-Permutations.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void permutations(vector<vector<int> >& res,
+                  vector<int> nums, int l, int h)
+{
+
+    if (l == h) {
+        res.push_back(nums);
+        return;
+    }
+
+    for (int i = l; i <= h; i++) {
+
+        swap(nums[l], nums[i]);
+
+        permutations(res, nums, l + 1, h);
+
+        swap(nums[l], nums[i]);
+    }
+}
+
+
+class Solution {
+    public:
+        vector<vector<int>> permute(vector<int>& nums) {
+            vector<vector<int> > res;
+            int x = nums.size() - 1;
+            permutations(res, nums, 0, x);
+            return res;
+        }
+};
+
+int main()
+{
+
+    return 0;
+}
+
